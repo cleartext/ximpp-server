5e0621d1de5d0bc8fb438a68aa35849d47544242
Alexander Artemenko <svetlyak.40wt@gmail.com>
2010-05-12 17:39:19 +0000

    Some logging and credentials setup for registrable component.

--- CheshiR-RegistrableComponent.py
+++ CheshiR-RegistrableComponent.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!bin/python
 
 import logging
 from SimpleBackend import SimpleBackend
@@ -6,15 +6,21 @@ from HTTPFrontend import HTTPFrontend
 from RegistrableComponent import RegistrableComponent
 
 # Uncomment the following line to turn on debugging
-#logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s %(message)s')
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)-8s %(message)s')
 
 def main() :
 	backend = SimpleBackend()
-	backend.jidToUser = {}
-	backend.userToJID = {}
+	backend.jidToUser = {
+        'user1@coolbananas.com.au': 'peter',
+        'user2@coolbananas.com.au': 'kevin',
+    }
+	backend.userToJID = {
+        'peter' : 'user1@coolbananas.com.au',
+        'kevin' : 'user2@coolbananas.com.au'
+      }
 	component = RegistrableComponent(
-		jid = "posts.cheshir.lit", password = "mypass",
-		server = "cheshir.lit", port = 5347, backend = backend)
+		jid = "microblog.coolbananas.com.au", password = "cleartext7u$",
+		server = "xmpp1.cleartext.im", port = 5349, backend = backend)
 	component.start()
 	httpFrontend = HTTPFrontend(8080, backend)
 	httpFrontend.start()
