35f9c235a4531ec28c2cc1476adee7bd95b72b06
Alexander Artemenko <svetlyak.40wt@gmail.com>
2010-06-03 19:01:57 +0000

    All code and confis were moved to separate directories. Bootstrap script updated to create two buildouts, forthe python and for the bot.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,36 @@
 #!/bin/bash
 
-~/devel/bin/python bootstrap.py --distribute
-bin/buildout -vv
+BASE=$(readlink -f $(dirname $0))
+VERBOSITY=-v
+
+if [ ! -d "$BASE/python" ]; then
+    echo ================================
+    echo Creating buildout for python
+    echo ================================
+
+    mkdir "$BASE/python"
+    ln -s "$BASE/configs/buildout/buildout_python.cfg" "$BASE/python/buildout.cfg"
+    python "$BASE/bootstrap.py" --distribute -c "$BASE/python/buildout.cfg"
+fi
+
+echo ================================
+echo Updating python buildout
+echo ================================
+"$BASE/python/bin/buildout" $VERBOSITY -c "$BASE/python/buildout.cfg"
+
+
+if [ ! -d "$BASE/env" ]; then
+    echo ================================
+    echo Creating buildout for bot
+    echo ================================
+
+    mkdir "$BASE/env"
+    ln -s "$BASE/configs/buildout/buildout.cfg" "$BASE/env/buildout.cfg"
+    "$BASE/python/bin/python" "$BASE/bootstrap.py" --distribute -c "$BASE/env/buildout.cfg"
+fi
+
+echo ================================
+echo Updating bot buildout
+echo ================================
+"$BASE/env/bin/buildout" $VERBOSITY -c "$BASE/env/buildout.cfg"
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'cleartext-bot',
+    version = '0.1.0',
+    description = 'Cleartext microblogging XMPP bot',
+    author = 'Alexander Artemenko',
+    packages = find_packages(),
+    zip_safe = True,
+)
+
