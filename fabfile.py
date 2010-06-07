1824679c27ad2caea832fbd18a8161d103e8aa7c
Alexander Artemenko <svetlyak.40wt@gmail.com>
2010-06-07 08:19:07 +0000

    Added configs for supervisord.

--- fabfile.py
+++ fabfile.py
@@ -21,6 +21,7 @@ def deploy():
     sudo('apt-get --yes install make')
 
     _pull_sources()
+    _update_supervisord()
     _update_buildout()
     # TODO: restart service
 
@@ -44,3 +45,20 @@ def _update_buildout():
     with cd(os.path.join(env.install_path, 'ximpp-server')):
         run('./bootstrap.sh')
 
+
+def _update_supervisord():
+    sudo('mkdir -p ~/log')
+    sudo('mkdir -p ~/run')
+    sudo('mkdir -p ~/etc/supervisor.d')
+
+    configs = os.path.join(
+        env.install_path, 'ximpp-server', 'configs'
+    )
+    svisor_main = '~/env/supervisord.conf'
+    svisor_main_dist = os.path.join(configs, 'supervisord', 'common.conf')
+
+    svisor_bot = '~/env/supervisor.d/bot.conf'
+    svisor_bot_dist = os.path.join(configs, 'supervisord', env.host + '.conf')
+
+    sudo('test -e %(svisor_main)s || ln -s %(svisor_main_dist)s %(svisor_main)s' % locals())
+    sudo('test -e %(svisor_bot)s || ln -s %(svisor_bot_dist)s %(svisor_bot)s' % locals())
