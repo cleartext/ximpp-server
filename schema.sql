b9cd31b48343d5ac57748bcd9c856af0cbd3879b
Alexander Artemenko <svetlyak.40wt@gmail.com>
2010-06-07 07:40:38 +0000

    Schema updated.

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE subscribers(
     user VARCHAR(250) NOT NULL,
     subscriber VARCHAR(250) NOT NULL,
     PRIMARY KEY (user, subscriber)
-);
+) ENGINE=InnoDB, CHARSET=utf8;
 
 -- add jid and presence fields
 ALTER TABLE users ADD jid VARCHAR(255) DEFAULT '', ADD presence BOOLEAN DEFAULT FALSE;
